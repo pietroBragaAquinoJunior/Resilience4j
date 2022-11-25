d0129cede1c6673e7e5751470df9b0c725ad2071
pietro <pietrobragaprofissional@gmail.com>
2022-11-25 19:20:29 -0300

    Sem Adding Retry Mechanism ainda

---
+++ ServiceA/src/main/java/com/dailycodebuffer/ServiceA/controller/ServiceAController.java
@@ -0,0 +1,32 @@
+package com.dailycodebuffer.ServiceA.controller;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/a")
+public class ServiceAController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String BASE_URL = "http://localhost:8081";
+
+    private static final String SERVICE_A = "serviceA";
+
+    @GetMapping
+    @CircuitBreaker(name = SERVICE_A, fallbackMethod = "serviceAFallBack")
+    public String serviceA(){
+        String url = BASE_URL + "/b";
+        return restTemplate.getForObject(url,String.class);
+    }
+
+    public String serviceAFallBack(Exception e){
+        return "this is a fallback method for Service A";
+    }
+
+}
